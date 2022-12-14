650a45a6ac9f4a751550d5877551d074e71ae455
spamfor <111302878+spamfor@users.noreply.github.com>
2022-12-13 20:44:05 -0500

    Create oppositeNumber.js
    cast num to opposite value, neg -> pos or pos -> neg

---
+++ oppositeNumber.js
@@ -0,0 +1,22 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+// My solution
+function opposite(number) {
+    if(number > 0){
+      return -Math.abs(number)
+    } else {
+      return Math.abs(number)
+     }
+  }
+
+// best solution
+function opposite(number){
+    return (-number);
+}
